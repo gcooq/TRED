081a3fc93fe4658466c3967f55bb892166f9e15e
gcooq <1152209908@qq.com>
2018-12-15 17:57:24 +0800

    Edin code-first commit

---
+++ tf_seq2seq_chatbot(edin)/tf_seq2seq_chatbot/data/train/20170329Edin_random_num3/skspilt.py
@@ -0,0 +1,28 @@
+from sklearn.cross_validation import train_test_split
+
+loadfile = open('Edinchat.txt','r')
+ratio = 0.1
+savefiletrain = open('Edinchat_train.txt','a+')
+savefiletest = open('Edinchat_test.txt','a+')
+random_num=3
+
+X = []
+y = []
+i = 1
+for line in loadfile:
+    if i%2 == 1:
+        X.append(line)
+    else:
+        y.append(line)
+    i += 1
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ratio,random_state=random_num)
+
+j = 0
+for x in X_train:
+    savefiletrain.write(x+y_train[j])
+    j += 1
+
+j = 0
+for x in X_test:
+    savefiletest.write(x+y_test[j])
+    j += 1
